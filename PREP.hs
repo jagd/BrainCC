8f0135c5180fd12b03275f1d35016514cb9dd457
wu <wuchuanren@gmail.com>
2012-01-19 02:00:13 +0100

    modified:   PREP.hs

--- PREP.hs
+++ PREP.hs
@@ -1,4 +1,8 @@
 {- |
+        It is saddly, that Parsec version 2 do not has Transformer !
+        Compiling Parsec 3 was not possible.
+        So i would write my own tokenizer to avoid unsafePerformIO.
+
         C Preprocessor
                 supported patterns:
                     #include
@@ -11,6 +15,8 @@
                     #ifndef M
                     #else
                     #endif
+
+                    THIS VERSION OF PREPROCESSOR DO NOT SUPPORT #if
 
                 supported comment style:
                     \/**\/ and \/\/
@@ -33,6 +39,12 @@
 -}
 
 module PREP where
+
+import Control.Monad.State
+import Control.Monad.Writer
+import Control.Monad.Error
+import qualified Data.Map as M
+
 
 -- | remove \/* .. *\/ comments
 removeCommentBlock :: String -> String
@@ -67,3 +79,87 @@ isSpace' '\r' = True
 isSpace' '\t' = True
 isSpace' ' ' = True
 isSpace' _ = False
+
+
+data PrepToken = Identifier String
+               -- ^ inclusive ID and keyword
+               | INCLUDE
+               -- ^ #include
+               | DEFINE
+               -- ^ #define
+               | IF
+               -- ^ #if
+               | IFDEF
+               -- ^ #ifdef
+               | IFNDEF
+               -- ^ #ifndef
+               | ELSE
+               -- ^ #else
+               | ENDIF
+               -- ^ #endif
+               | LineContinue
+               -- ^ \\'NewLine' at the end of a line , only in #define
+               | NewLine
+               -- ^ regex: [\\r\\n]* will reset the flag for #commands
+               | RawString String
+               -- ^ c string with \"\" and escape \\
+               | RawChar String
+               -- ^ c char with \'\' and escape \\
+               | COMMA
+               -- ^ the , char
+               | OtherToken String
+               -- ^ seperate by spaces or nonspaces
+               | EOF
+
+data PrepState = PrepState {
+                         restCode    :: String,
+                         lineNumber  :: Int,
+                         isLineBegin :: Bool,
+                         defineTree  :: M.Map String String
+                 }
+
+type PrepParser a = ErrorT String (StateT PrepState (WriterT String IO)) a
+
+_azAZ = ['a'..'z'] ++ ['A'..'Z'] ++ ['_']
+_azAZ09 = ['a'..'z'] ++ ['A'..'Z'] ++ ['_'] ++ ['0'..'9']
+
+token :: PrepParser PrepToken
+token = get >>= return . restCode
+            >>= \code -> invokeToken code
+        where
+          invokeToken [] = return EOF
+          invokeToken (x:xs)
+                  | x == '#'
+                      = get >>= return . isLineBegin >>=
+                      \lb -> if lb
+                               then tokenMacro
+                               else do
+                                    l <- getLineNumber
+                                    throwError ("illegal # at line " ++ show l)
+                  | x `elem` _azAZ
+                          = tokenIdentifier
+                  | x == '\\' && (not $ null xs) && (head xs) `elem` "\r\n"
+                          = tokenLineContinue
+                  | x == '"' = tokenRawString
+                  | x == '\'' = tokenRawChar
+                  | x `elem` "\r\n" = tokenNewLine
+                  | x == ',' = do
+                               skipCode 1
+                               tell " "
+                               return COMMA
+                  | otherwise = tokenOther
+
+
+getLineNumber :: PrepParser Int
+getLineNumber = get >>= return . lineNumber
+
+skipCode :: Int -> PrepParser ()
+skipCode offset = undefined
+
+tokenNewLine = undefined
+tokenIdentifier = undefined
+tokenLineContinue = undefined
+tokenRawChar = undefined
+tokenRawString = undefined
+tokenMacro = undefined
+tokenOther = undefined
